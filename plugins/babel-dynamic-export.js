7c3140119ffb56d258233159f198217a3b6a39d5
duanbowen <dbwencookie@gmail.com>
2019-07-17 23:29:55 +0800

    chore: update harcode babel

--- plugins/babel-dynamic-export.js
+++ plugins/babel-dynamic-export.js
@@ -6,24 +6,25 @@ const template = require('@babel/template').default; //
 
 const isSymbol = Symbol('symbol');
 const code = `
-    const arr = ["setState", "getName"];
-    const Actions = actionsFactory(arr, 'home');
-    export default Actions;
+    import { actionsFactory } from 'tila'
+    const actions = ["setState", "getName"];
+    const actionHelper = actionsFactory(actions, 'home')
+    export default actionHelper;
 `;
 
-// const code = `
-//     const arr = ["setState", "getName"];
-//     export default actionsFactory(arr, 'home');
-// `;
 function markVisited(node) {
     node[isSymbol] = true;
     return node;
 }
 
-// 生成 多个 export const setState = Actions.setState;
-// arr = ['setState', 'getName'], prefixName = Actions;
-function buildNamedExports(arr, prefixName) {
+function buildNamedExports(arr, prefixName, path) {
+    const names = [];
     const exportedNames =  arr.map((key) => {
+    
+    // 生成 actinsNames 对象
+    const property = t.objectProperty(t.identifier(key), t.identifier(key));
+    names.push(property);
+
     // exportSpecifier 必须要两个参数 local,exported;
     // if local === exported, the result is export { exported }
     return markVisited(
@@ -31,69 +32,66 @@ function buildNamedExports(arr, prefixName) {
             t.identifier(`${prefixName}.${key}`),
             t.identifier(key)
         ));
-    })
-    return markVisited(
-        t.exportNamedDeclaration(
+    });
+    // const actionsNamesVarUid = path.scope.generateUidIdentifier('actionNames');
+    // const action = t.objectExpression(names);
+    // { setState: 'setState', getName: 'getName' };
+    // const actionsNames = t.variableDeclarator(actionsNamesVarUid, action)
+    exportedNames.push(
+        t.exportSpecifier(
+            t.identifier(`${prefixName}.actionNames`),
+            t.identifier('actionNames')
+        )
+    );
+    return t.exportNamedDeclaration(
             null,
             exportedNames
         )
-    )
 }
-
-
 
 const ast = parser.parse(
     code,
     { sourceType: 'module' } // 支持 export/import
 );
 traverse(ast, {
-    ExportDefaultDeclaration(path) {
-        const defaultIdentifier = t.identifier('default');
+    FunctionDeclaration(path) {
         const declaration = path.node.declaration;
-        const isIdentifier = t.isIdentifier(declaration); // export default Actions
-        const isCallExpression = t.isCallExpression(declaration);// export default actionsFactory([], name)
-        let binding = '';
-        if(isIdentifier) {
-            binding = path.scope.getBinding(declaration.name);
-        } else if (isCallExpression && declaration.callee.name === 'actionsFactory') {
-            binding = path.scope.getBinding(declaration.callee.name);
-        }
-        // 
-        path.addComment('leading', ' generate by babel plugin ');
-
-        const newNode = t.exportSpecifier(t.identifier('test'), t.identifier('test'));
-        path.insertAfter(t.expressionStatement(t.stringLiteral("A little high, little low.")))
-        path.insertAfter(newNode)
-
-        const id = declaration.id || path.scope.generateUidIdentifier('default');
-        const exportedNames = buildNamedExports(['setState', 'getName'], 'Actions');
-        path.insertAfter(exportedNames)
-        // path.get('body').pushContainer('body', exportedNames);
-
-        // path.replaceWithMultiple([
-        //     declaration,
-        //     buildNamedExport(id, defaultIdentifier)
-        //   ]);
-        // const isImmutable = !binding || ['const', 'module'].includes(binding.kind);
-      }
+        console.log("log: FunctionDeclaration -> declaration", declaration)
+    },
+    
+    ExportDefaultDeclaration : {
+        enter(path) {
+            const declaration = path.node.declaration;
+            const nodeName = declaration.name;
+            let effectsName = [];
+            if(nodeName === 'actionHelper') {
+                let container = path.container;
+                // 找到 actions 对象
+                container.forEach(element => {
+                    const declarations = element.declarations;
+                    if(element.type === 'VariableDeclaration' && declarations.length) {
+                        declarations.forEach(ele => {
+                            if(ele.id.name === 'actions') {
+                                effectsName = ele.init.elements.map(o => o.value);
+                            }
+                        });
+                    }
+                });
+            }
+            const name = declaration.name;
+            path.addComment('leading', ' generate by babel plugin ');
+            const exportedNames = buildNamedExports(effectsName, name, path);
+            path.insertAfter(exportedNames);
+          },
+    },
+    FunctionDeclaration(path) {
+        console.log("log: FunctionDeclaration -> path", path)
+    }
 });
 
-console.log(generate(ast).code);
+const codes = generate(ast).code;
+console.log('codes: ', codes)
 
-const ge = generate(ast, {}, code);
-console.log('ge: ', ge);
-
-const buildRequire = template(`
-  var IMPORT_NAME = require(SOURCE);
-`);
-
-const ast2 = buildRequire({
-  IMPORT_NAME: t.identifier("myModule"),
-  SOURCE: t.stringLiteral("my-module")
-});
-
-const code2 = generate(ast2).code;
-console.log('code2: ', code2);
 debugger;
 
 const updateParamNameVisitor = {
@@ -103,8 +101,6 @@ const updateParamNameVisitor = {
       }
     }
   };
-
-  
 function DynamicExport(babel) {
     console.log('babel: ', babel);
     const { types: t, template, path } = babel;
